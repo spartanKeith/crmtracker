5adfafc9b08fb97bd4462ce5bb3da3676a39a644
spartanKeith <respectthegoatok@gmail.com>
2026-05-12 15:48:17 +0800

    update

--- environment.js
+++ environment.js
@@ -14,8 +14,8 @@ window.APP_ENV = {
   ACCOUNTS: [
     { username: 'spartan_chard', password: 'keithpogi', role: 'super_admin', display: 'Chard' },
     { username: 'spartan_keith', password: 'keithpogi', role: 'super_admin', display: 'Keith' },
-    { username: 'spartan_gift', password: 'keithpogi', role: 'super_admin', display: 'Gifthel' },
-    { username: 'spartan_rich', password: 'keithpogi', role: 'super_admin', display: 'Richelle' }
+    { username: 'spartan_gift', password: 'keithpogi', role: 'viewer', display: 'Gifthel' },
+    { username: 'spartan_rich', password: 'keithpogi', role: 'viewer', display: 'Richelle' }
   ],
   MEMBER_ACCOUNT_ROLES: [
     { value: 'viewer', label: 'Viewer' },
